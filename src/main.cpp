f2661d88143ee5d0c356201afe84bf97f3b1463a
crystal <2391434441@qq.com>
2022-04-02 14:01:29 +0800

    master

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int pin = 2;
+
+void setup()
+{
+  pinMode(pin , OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(pin , HIGH);
+
+  delay(1000);
+
+  digitalWrite(pin , LOW );
+  delay(1000);
+}
